fc78fff3669e14a80643f601fab7559904b9610d
LiuWenYuan <15816141883@163.com>
2019-11-23 07:50:49 +0800

    添加项目文件。

---
+++ Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DPIAwareTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.DpiChanged += WhenDpiChanged;
+            label3.Text = this.DeviceDpi + " DPI";
+        }
+
+        private void WhenDpiChanged(object sender, DpiChangedEventArgs e)
+        {
+            label3.Text = this.DeviceDpi + " DPI";
+        }
+    }
+}
